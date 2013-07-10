ff2d50f4f8819423f8d058f1aec26869131b04d4
Barry Allahyar <barrya@gmail.com>
2013-07-10 13:28:30 -0500

    Bitstarter update

--- web.js
+++ web.js
@@ -1,12 +1,15 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var fs = require('fs');
+  //buf = new Buffer(256);
+  buffer = fs.readFileSync('index.html','utf-8').toString();
+  response.send(buffer);//buf.toString('utf8',0,len));
+  //console.log(buffer);
 });
-
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+	console.log("Listening on " + port);
 });
+
